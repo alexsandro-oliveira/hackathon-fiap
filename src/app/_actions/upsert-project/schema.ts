2fd7e33e8f5c2b362fb74a0901d2e01fc3ea5053
Alexs <ale.osantos@hotmail.com>
2025-02-05 21:48:37 +0000

    feat: add schema upsert project

--- src/app/_actions/upsert-project/schema.ts
+++ src/app/_actions/upsert-project/schema.ts
@@ -1,7 +1,11 @@
+import { DeadlineOpton } from '@prisma/client'
 import { z } from 'zod'
 
 export const upsertProjectSchema = z.object({
   name: z.string().trim().min(1),
   description: z.string().trim().min(5),
   fornecedor: z.string().trim().min(1),
+  phones: z.string().trim().min(1),
+  region: z.string().trim().min(1),
+  deadline: z.nativeEnum(DeadlineOpton),
 })
